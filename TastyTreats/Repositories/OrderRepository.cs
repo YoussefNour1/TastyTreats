e3b9f4740ffb126b5814bd1b8c8de7a05f02e5f1
abdelrahman-sedeek <67134107+abdelrahman-sedeek@users.noreply.github.com>
2024-09-08 09:00:27 +0300

    Create Repository folder

---
+++ TastyTreats/Repositories/OrderRepository.cs
@@ -0,0 +1,38 @@
+﻿using TastyTreats.Contexts;
+using TastyTreats.Models;
+
+namespace TastyTreats.Repositories
+{
+    public class OrderRepository
+    {
+         TastyTreatsContext context;
+
+        //CRUD 
+        public OrderRepository()
+        {
+            context = new TastyTreatsContext();    
+        }
+        public void Add( Order order)
+        {
+            context.Add(order);
+ 
+        }
+        public void Update(Order order) { 
+            context.Update(order);
+        }
+        public void Delete(int id) { 
+            var data=GetById(id);
+            context.Orders.Remove(data);
+        }
+        public Order GetById(int id) {
+            return context.Orders.FirstOrDefault(O => O.OrderId == id);
+        }
+        public List<Order> GetAll() {
+            return context.Orders.ToList();
+        }
+        public void Save()
+        {
+             context.SaveChanges();
+        }
+    }
+}
