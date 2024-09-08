4093a25a1e3e9380dc83a23bb2bda7bb0c8eafe8
Noha Gamal <120256757+NohaGamal118@users.noreply.github.com>
2024-09-08 23:08:57 +0300

    ItemRepository

--- TastyTreats/Program.cs
+++ TastyTreats/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using TastyTreats.Data;
+using TastyTreats.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,9 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+//register
+builder.Services.AddTransient<IItemRepository, ItemRepository>();
 
 var app = builder.Build();
 
---
+++ TastyTreats/Repositories/ItemRepository.cs
@@ -0,0 +1,52 @@
+﻿using TastyTreats.Contexts;
+using TastyTreats.Models;
+
+namespace TastyTreats.Repositories
+{
+    public class ItemRepository : IItemRepository
+    {
+        TastyTreatsContext context;
+
+        public ItemRepository()
+        {
+            context=new TastyTreatsContext();  
+        }
+
+     
+        // CRUD 
+
+        public void Add(Item item)
+        {
+            context.Items.Add(item);
+        }
+
+        public void Update(Item item)
+        {
+            context.Items.Update(item); 
+        }
+
+        public void Delete(int id)
+        {
+            var item = GetById(id);
+            if (item != null)
+            {
+                context.Items.Remove(item);
+            }
+        }
+
+        public Item GetById(int id)
+        {
+            return context.Items.FirstOrDefault(i => i.ItemId == id);
+        }
+
+        public List<Item> GetAll()
+        {
+            return context.Items.ToList();
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+    }
+}
