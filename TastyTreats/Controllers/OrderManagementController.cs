3ddca963a7bc54fca5c72189bbde3a6b50c2c142
abdelrahman-sedeek <67134107+abdelrahman-sedeek@users.noreply.github.com>
2024-10-07 20:17:48 +0300

    orderMamegment

---
+++ TastyTreats/Controllers/OrderManagementController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TastyTreats.Controllers
+{
+    //[Authorize(Roles = "admin")]
+
+    public class OrderManagementController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
